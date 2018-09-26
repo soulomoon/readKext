33424f2b9afd5af1e2a1114f156e20845c4155e8
soulomoon <fwy996602672@gmail.com>
2018-09-26 19:22:50 +0800

    add option to run

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # readKext  
+## option1
 把 read.py 放在放有kext的文件夹下面  
 然后在terminal下面cd到那个文件夹  
 运行`python3 read.py`  
+
+## option2
+直接运行, 例如
+`python3 read.py /Volumes/ESP/EFI/CLOVER/kexts/10.14`
+
--- read.py
+++ read.py
@@ -1,26 +1,33 @@
 import pathlib
+import os
+import sys
 from xml.dom import minidom
 
-dp = pathlib.Path()
-for f in sorted(list(dp.iterdir())):
-    if f.match("*.kext"):
-        if not f.is_dir():
-            continue
-        print(f)
-        for content in f.iterdir():
-            if not content.is_dir():
+if __name__ == "__main__":
+    path = "."
+    if len(sys.argv)>1: 
+        path = sys.argv[1]
+
+    dp = pathlib.Path(path)
+    for f in sorted(list(dp.iterdir())):
+        if f.match("*.kext"):
+            if not f.is_dir():
                 continue
-            for info in content.iterdir():
-                if info.match("*.plist"):
-                    try:
-                        with info.open() as fp:
-                            doc = minidom.parse(fp)
-                            items = doc.getElementsByTagName('dict')
-                            d = items[0]
-                            keys = [i.firstChild.data for i in d.getElementsByTagName("key")]
-                            vals = [i.firstChild.data for i in
-                                    d.getElementsByTagName("string")]
-                            pairs = dict(zip(keys, vals))
-                            print(pairs["CFBundleShortVersionString"])
-                    except:
-                        pass
+            print(f.name)
+            for content in f.iterdir():
+                if not content.is_dir():
+                    continue
+                for info in content.iterdir():
+                    if info.match("*.plist"):
+                        try:
+                            with info.open() as fp:
+                                doc = minidom.parse(fp)
+                                items = doc.getElementsByTagName('dict')
+                                d = items[0]
+                                keys = [i.firstChild.data for i in d.getElementsByTagName("key")]
+                                vals = [i.firstChild.data for i in
+                                        d.getElementsByTagName("string")]
+                                pairs = dict(zip(keys, vals))
+                                print(pairs["CFBundleShortVersionString"])
+                        except:
+                            pass
