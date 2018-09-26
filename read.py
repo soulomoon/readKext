c8cfe8e3e1e4cf834e8be5fee125ee25467c7c8f
soulomoon <fwy996602672@gmail.com>
2018-09-26 18:56:10 +0800

    fix error

--- read.py
+++ read.py
@@ -4,16 +4,23 @@ from xml.dom import minidom
 dp = pathlib.Path()
 for f in dp.iterdir():
     if f.match("*.kext"):
+        if not f.is_dir():
+            continue
         print(f)
         for content in f.iterdir():
+            if not content.is_dir():
+                continue
             for info in content.iterdir():
                 if info.match("*.plist"):
-                    with info.open() as fp:
-                        doc = minidom.parse(fp)
-                        items = doc.getElementsByTagName('dict')
-                        d = items[0]
-                        keys = [i.firstChild.data for i in d.getElementsByTagName("key")]
-                        vals = [i.firstChild.data for i in
-                                d.getElementsByTagName("string")]
-                        pairs = dict(zip(keys, vals))
-                        print(pairs["CFBundleShortVersionString"])
+                    try:
+                        with info.open() as fp:
+                            doc = minidom.parse(fp)
+                            items = doc.getElementsByTagName('dict')
+                            d = items[0]
+                            keys = [i.firstChild.data for i in d.getElementsByTagName("key")]
+                            vals = [i.firstChild.data for i in
+                                    d.getElementsByTagName("string")]
+                            pairs = dict(zip(keys, vals))
+                            print(pairs["CFBundleShortVersionString"])
+                    except:
+                        pass
