694e7dbf95e44f672c27640c5a042f697feb5b14
soulomoon <fwy996602672@gmail.com>
2018-09-26 19:40:48 +0800

    This is fast push

--- read.py
+++ read.py
@@ -8,16 +8,13 @@ if __name__ == "__main__":
         path = sys.argv[1]
 
     dp = pathlib.Path(path)
-    for f in sorted(list(dp.iterdir())):
-        if f.match("*.kext"):
-            if not f.is_dir():
-                continue
-            print(f.name)
-            with minidom.parse(str(f) + "/Contents/Info.plist") as doc:
-                items = doc.getElementsByTagName('dict')
-                d = items[0]
-                keys = [i.firstChild.data for i in d.getElementsByTagName("key")]
-                vals = [i.firstChild.data for i in
-                        d.getElementsByTagName("string")]
-                pairs = dict(zip(keys, vals))
-                print(pairs["CFBundleShortVersionString"])
+    for f in sorted(i for i in dp.iterdir() if i.match("*.kext") and i.is_dir()):
+        print(f.name)
+        with minidom.parse(str(f) + "/Contents/Info.plist") as doc:
+            items = doc.getElementsByTagName('dict')
+            d = items[0]
+            keys = [i.firstChild.data for i in d.getElementsByTagName("key")]
+            vals = [i.firstChild.data for i in
+                    d.getElementsByTagName("string")]
+            pairs = dict(zip(keys, vals))
+            print(pairs["CFBundleShortVersionString"])
